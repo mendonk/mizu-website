650480c645fdc1b08413bfaabe3fa936f0c2326d
Neim <elezin9@gmail.com>
2021-06-22 18:54:01 +0200

    redirect issue solved

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,7 @@ exports.createPages = ({ actions }) => {
     const { createRedirect } = actions;
     createRedirect({
         fromPath: "/mizu-website",
-        toPath: "/home",
+        toPath: "/",
+        isPermanent: true,
     });
 };
